db56df50f387ff37988bbbd20570c48979bede5b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-27 13:41:14 +0700

    connection

--- admin/modal/user/create.php
+++ admin/modal/user/create.php
@@ -1,14 +1,14 @@
 <div class="modal fade" id="addUser" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true">
    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-         <div class="modal-header">
-            <h5 class="modal-title" id="addUserModalLabel">Tambah User</h5>
-            <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-            </button>
-         </div>
-         <div class="modal-body">
-            <form action="" method="post">
+      <form action="" method="post">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title" id="addUserModalLabel">Tambah User</h5>
+               <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
                <div class="mb-3">
                   <label for="name">Nama</label>
                   <input type="text" name="name" id="name" class="form-control" required>
@@ -33,12 +33,12 @@
                      <option value="user">User</option>
                   </select>
                </div>
-            </form>
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Tutup</button>
+               <button type="submit" class="btn bg-gradient-primary">Simpan</button>
+            </div>
          </div>
-         <div class="modal-footer">
-            <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn bg-gradient-primary">Save changes</button>
-         </div>
-      </div>
+      </form>
    </div>
 </div>
--- admin/users.php
+++ admin/users.php
@@ -8,6 +8,33 @@
                <button class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#addUser">Tambah User</button>
             </h4>
          </div>
+         <div class="card-body">
+            <div class="table-responsive">
+               <table class="table align-items-center mb-0">
+                  <thead>
+                     <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Telepon</th>
+                        <th>Aksi</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                        <td>1</td>
+                        <td>Faqih</td>
+                        <td>l6n3n@example.com</td>
+                        <td>08123456789</td>
+                        <td>
+                           <button class="btn btn-warning btn-sm">Edit</button>
+                           <button class="btn btn-danger btn-sm">Hapus</button>
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+         </div>
       </div>
    </div>
 </div>
