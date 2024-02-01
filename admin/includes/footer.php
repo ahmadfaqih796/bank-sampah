2049051288a5113c3056a1b71ac71e37a1d0d31b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-02-01 08:55:27 +0700

    change title

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,7 +7,7 @@
                   document.write(new Date().getFullYear())
                </script>,
                made with <i class="fa fa-heart"></i> by
-               <a href="" class="font-weight-bold" target="_blank">Bank Sampah</a>
+               <a href="" class="font-weight-bold" target="_blank">Bank Sampah Asri Mandiri</a>
                for a better web.
             </div>
          </div>
--- user/includes/sidebar.php
+++ user/includes/sidebar.php
@@ -2,7 +2,7 @@
    <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="index.php">
-         <h4>Bank Sampah</h4>
+         <h4>Bank Sampah Asri Mandiri</h4>
       </a>
    </div>
    <hr class="horizontal dark mt-0">
